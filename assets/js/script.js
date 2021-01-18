0a7cb2d19fcbd04e1d559123a1e582f33dd3b62b
Joseph Prospero <jprospero413@gmail.com>
2021-01-17 18:37:26 -0800

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,15 +137,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     //array to store the task data in
@@ -181,12 +187,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     ui.draggable.remove();
     console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -210,6 +219,12 @@ var auditTask = function(taskEl) {
   }
 };
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
+
 $("#modalDueDate").datepicker({
   minDate: 1,
   onClose: function() {
@@ -230,7 +245,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -259,8 +274,6 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
-
-
 
 // load tasks for the first time
 loadTasks();
